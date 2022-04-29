3d2f151402df53b7308441ab34a37d908087623b
Rory Grant <rorygran5@gmail.com>
2022-04-29 11:36:49 -0400

    commit tree 7724f42ddc1ad959f0a8c7919e724abaf6050376

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.removeChild(document.body.querySelector("main"))
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Rory is the champion";
+document.body.append(newHeader);
